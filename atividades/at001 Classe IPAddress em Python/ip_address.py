8914297a84fd849d6c42ee79c7a6834c15b16fc3
Thiago Gonsalves Da Silva <silva.gonsalves@academico.ifrn.edu.br>
2025-10-22 12:04:19 -0300

    feito interface para testar

--- atividades/at001 Classe IPAddress em Python/ip_address.py
+++ atividades/at001 Classe IPAddress em Python/ip_address.py
@@ -92,3 +92,92 @@ print(teste.broadcast)
 print(teste)      
 print(teste.pertence_a_rede("192.168.1.55"))  # True
 print(teste.pertence_a_rede("192.168.2.1"))   # False
+
+class UI:
+    def __init__(self):
+        self.endereco = None
+        self.opcao = 0
+        self.msg = ""
+
+    def main(self):
+        rodando = True
+        while rodando:
+            try:
+                if self.opcao == 0:
+                    self.selecao()
+                elif self.opcao == 1:
+                    self.criar_rede()
+                elif self.opcao == 2:
+                    self.ver_CIDR()
+                elif self.opcao == 3:
+                    self.ver_ip()
+                elif self.opcao == 4:
+                    self.ver_mascara()
+                elif self.opcao == 5:
+                    self.ver_rede()
+                elif self.opcao == 6:
+                    self.ver_broadcast()
+                elif self.opcao == 7:
+                    self.testar_ip_pertence_rede()
+                elif self.opcao == -1:
+                    rodando = False
+            except:
+                self.opcao = 0
+                print("Ocorreu algum erro")
+    
+    def selecao(self):
+        print("Digite:")
+        ops = 7
+        print("    \"1\" | Para criar rede")
+        if self.endereco != None:
+            print("    \"2\" | Para ver CIDR")
+            print("    \"3\" | Para ver ip")
+            print("    \"4\" | Para ver mascara")
+            print("    \"5\" | Para ver rede")
+            print("    \"6\" | Para ver broadcast")
+            print("    \"7\" | Para verificar se ip exite na rede")
+        print("    \"-1\" | Para sair do sistema")
+
+        if self.msg != "":
+            print(self.msg)
+
+        op = input("Digite aqui: ")
+        print(op)
+        if op == None or op == "":
+            self.msg = ""
+        else:
+            self.opcao = int(op) if -1 <= int(op) <= ops else 0
+
+    def criar_rede(self):
+        ip = input("Digite um ip: ")
+        mascara = input("Digite a mascara da rede: ")
+        self.endereco = IPAddress(ip, mascara)
+        self.opcao = 0
+
+    def ver_CIDR(self):
+        self.msg = self.endereco
+        self.opcao = 0
+
+    def ver_ip(self):
+        self.msg = self.endereco.IPv4
+        self.opcao = 0
+    
+    def ver_mascara(self):
+        self.msg = self.endereco.mascara
+        self.opcao = 0
+
+    def ver_rede(self):
+        self.msg = self.endereco.rede
+        self.opcao = 0
+    
+    def ver_broadcast(self):
+        self.msg = self.endereco.broadcast
+        self.opcao = 0
+
+    def testar_ip_pertence_rede(self):
+        pertence = self.endereco.pertence_a_rede(input("Digite um ip: "))
+        self.msg = "Pertence" if pertence else "Não Pertence"
+        self.opcao = 0
+
+ui = UI()
+ui.main()
